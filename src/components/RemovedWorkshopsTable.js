627807382de479d1fda6e6af3fb412ed3a36cfa1
KazeIU <calebfinley989@gmail.com>
2021-01-26 17:47:40 +0900

    Remove Days and Colors from Sorting

--- src/components/RemovedWorkshopsTable.js
+++ src/components/RemovedWorkshopsTable.js
@@ -95,18 +95,23 @@ function EnhancedTableHead(props) {
             className="table-header-text"
             sortDirection={orderBy === headCell.id ? order : false}
           >
-            <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : "asc"}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <span className={classes.visuallyHidden}>
-                  {order === "desc" ? "sorted descending" : "sorted ascending"}
-                </span>
-              ) : null}
-            </TableSortLabel>
+            {headCell.id === "colors" || headCell.id === "days"? 
+              <div>
+                {headCell.label}
+              </div>:
+              <TableSortLabel
+                active={orderBy === headCell.id}
+                direction={orderBy === headCell.id ? order : "asc"}
+                onClick={createSortHandler(headCell.id)}
+              >
+                {headCell.label}
+                {orderBy === headCell.id ? (
+                  <span className={classes.visuallyHidden}>
+                    {order === "desc" ? "sorted descending" : "sorted ascending"}
+                  </span>
+                ) : null}
+              </TableSortLabel>
+            }
           </TableCell>
         ))}
       </TableRow>
